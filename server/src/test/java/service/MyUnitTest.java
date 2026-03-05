526901fa5067bc3a99fe56e6aa0da27acf1dc1ce
Kevin Pena <kevinmpena05@gmail.com>
2026-03-04 20:28:14 -0700

    Code Quality Check #4

--- server/src/test/java/service/MyUnitTest.java
+++ server/src/test/java/service/MyUnitTest.java
@@ -5,45 +5,41 @@ import passoff.model.*;
 import passoff.server.TestServerFacade;
 import server.Server;
 
-import java.net.HttpRetryException;
-import java.net.HttpURLConnection;
-import java.util.*;
-
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MyUnitTest {
 
-    private static TestUser existingUser;
-    private static TestUser newUser;
-    private static TestCreateRequest createRequest;
-    private static TestServerFacade serverFacade;
-    private static Server server;
-    private String existingAuth;
+    private static TestUser existingUserTest;
+    private static TestUser newUserTest;
+    private static TestCreateRequest createRequestTest;
+    private static TestServerFacade serverFacadeTest;
+    private static Server serverTest;
+    private String existingAuthTest;
 
     // ### TESTING SETUP/CLEANUP ###
 
     @AfterAll
     static void stopServer() {
-        server.stop();
+        serverTest.stop();
     }
 
     @BeforeAll
     public static void initTest() {
-        server = new Server();
-        var port = server.run(0);
+        serverTest = new Server();
+        var port = serverTest.run(0);
         System.out.println("Started test HTTP server on " + port);
-        serverFacade = new TestServerFacade("localhost", Integer.toString(port));
-        existingUser = new TestUser("ExistingUser", "existingUserPassword", "eu@mail.com");
-        newUser = new TestUser("NewUser", "newUserPassword", "nu@mail.com");
-        createRequest = new TestCreateRequest("testGame");
+        serverFacadeTest = new TestServerFacade("localhost", Integer.toString(port));
+        existingUserTest = new TestUser("ExistingUser", "existingUserPassword", "eu@mail.com");
+        newUserTest = new TestUser("NewUser", "newUserPassword", "nu@mail.com");
+        createRequestTest = new TestCreateRequest("testGame");
     }
 
     @BeforeEach
     public void setup() {
-        serverFacade.clear();
+        serverFacadeTest.clear();
 
         //one user already logged in
-        TestAuthResult regResult = serverFacade.register(existingUser);
-        existingAuth = regResult.getAuthToken();
+        TestAuthResult regResult = serverFacadeTest.register(existingUserTest);
+        existingAuthTest = regResult.getAuthToken();
     }
 
 
@@ -55,7 +51,7 @@ public class MyUnitTest {
     @DisplayName("Register Success")
     public void registerSuccess(){
         //submit register request
-        TestAuthResult registerResult = serverFacade.register(newUser);
+        TestAuthResult registerResult = serverFacadeTest.register(newUserTest);
         Assertions.assertEquals(registerResult.getUsername(), registerResult.getUsername(),
                 "Response did not have the same username as was registered");
         Assertions.assertNotNull(registerResult.getAuthToken(), "Response did not contain an authentication string");
@@ -66,7 +62,7 @@ public class MyUnitTest {
     public void registerFail(){
         TestUser incompleteUser = new TestUser(null,"passwordTest","email@test.com");
         //submit register request
-        TestAuthResult registerResult = serverFacade.register(incompleteUser);
+        TestAuthResult registerResult = serverFacadeTest.register(incompleteUser);
         Assertions.assertNull(registerResult.getUsername());
     }
 
@@ -90,13 +86,13 @@ public class MyUnitTest {
     @Test
     @DisplayName("logout Success")
     public void logoutSuccess(){
-        TestResult logoutResult = serverFacade.logout(existingAuth);
+        TestResult logoutResult = serverFacadeTest.logout(existingAuthTest);
     }
     //logout NEGATIVE (Unauthorized)
     @Test
     @DisplayName("logout Fail")
     public void logoutFail(){
-        TestResult logoutResult = serverFacade.logout(null);
+        TestResult logoutResult = serverFacadeTest.logout(null);
         Assertions.assertEquals("Error: unauthorized",logoutResult.getMessage());
     }
 
@@ -105,14 +101,14 @@ public class MyUnitTest {
     @Test
     @DisplayName("CreateGame Success")
     public void createGameSuccess(){
-        TestCreateResult createResult = serverFacade.createGame(createRequest, existingAuth);
+        TestCreateResult createResult = serverFacadeTest.createGame(createRequestTest, existingAuthTest);
         Assertions.assertNotNull(createResult.getGameID());
     }
     //Create Game NEGATIVE
     @Test
     @DisplayName("CreateGame Success")
     public void createGameFail(){
-        TestCreateResult createResult = serverFacade.createGame(createRequest, null);
+        TestCreateResult createResult = serverFacadeTest.createGame(createRequestTest, null);
         Assertions.assertNull(createResult.getGameID());
     }
 
@@ -120,8 +116,8 @@ public class MyUnitTest {
     @Test
     @DisplayName("ListGames Success")
     public void listGamesSuccess(){
-        TestAuthResult userLogin = serverFacade.login(existingUser);
-        TestListResult listResult = serverFacade.listGames(userLogin.getAuthToken());
+        TestAuthResult userLogin = serverFacadeTest.login(existingUserTest);
+        TestListResult listResult = serverFacadeTest.listGames(userLogin.getAuthToken());
         Assertions.assertNotNull(listResult);
 
     }
@@ -130,7 +126,7 @@ public class MyUnitTest {
     @Test
     @DisplayName("ListGames Fail")
     public void listGamesFail(){
-        TestListResult listResult = serverFacade.listGames(null);
+        TestListResult listResult = serverFacadeTest.listGames(null);
         Assertions.assertEquals("Error: Unauthorized",listResult.getMessage());
 
     }
@@ -141,14 +137,14 @@ public class MyUnitTest {
     @DisplayName("Join Game Success")
     public void joinGameSuccess() {
         //create game
-        TestCreateResult createResult = serverFacade.createGame(createRequest, existingAuth);
+        TestCreateResult createResult = serverFacadeTest.createGame(createRequestTest, existingAuthTest);
         //join as Black
         TestJoinRequest joinRequest = new TestJoinRequest(ChessGame.TeamColor.BLACK, createResult.getGameID());
 
         //try join
-        TestResult joinResult = serverFacade.joinPlayer(joinRequest, existingAuth);
+        TestResult joinResult = serverFacadeTest.joinPlayer(joinRequest, existingAuthTest);
 
-        TestListResult listResult = serverFacade.listGames(existingAuth);
+        TestListResult listResult = serverFacadeTest.listGames(existingAuthTest);
 
         Assertions.assertNotNull(listResult.getGames(), "List result did not contain games");
     }
@@ -159,7 +155,7 @@ public class MyUnitTest {
     public void joinGameNegative(){
         //join as Black
         TestJoinRequest joinRequest = new TestJoinRequest(ChessGame.TeamColor.BLACK, null);
-        TestResult joinResult = serverFacade.joinPlayer(joinRequest,null);
+        TestResult joinResult = serverFacadeTest.joinPlayer(joinRequest,null);
         Assertions.assertEquals("Error: Unauthorized",joinResult.getMessage());
     }
 
@@ -168,13 +164,13 @@ public class MyUnitTest {
     @DisplayName("Clear function")
     public void clearSuccess(){
         //create filler games
-        serverFacade.createGame(new TestCreateRequest("Test1"), existingAuth);
-        serverFacade.createGame(new TestCreateRequest("Test2"), existingAuth);
+        serverFacadeTest.createGame(new TestCreateRequest("Test1"), existingAuthTest);
+        serverFacadeTest.createGame(new TestCreateRequest("Test2"), existingAuthTest);
 
         //do clear
-        TestResult clearResult = serverFacade.clear();
-        TestAuthResult authData = serverFacade.register(newUser);
-        TestListResult list = serverFacade.listGames(authData.getAuthToken());
+        TestResult clearResult = serverFacadeTest.clear();
+        TestAuthResult authData = serverFacadeTest.register(newUserTest);
+        TestListResult list = serverFacadeTest.listGames(authData.getAuthToken());
 
         //check listResult
         Assertions.assertNotNull(list.getGames(), "List result did not contain an empty list of games");
