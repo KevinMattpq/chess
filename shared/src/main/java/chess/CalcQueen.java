bf9f6fbb69465dfc845efc70c084067e203fd003
Kevin Pena <kevinmpena05@gmail.com>
2026-03-09 21:00:26 -0600

    Code Quality Check

--- shared/src/main/java/chess/CalcQueen.java
+++ shared/src/main/java/chess/CalcQueen.java
@@ -14,6 +14,74 @@ public class CalcQueen extends PieceMovesCalculator {
         }
     }
 
+    @Override
+    public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position){
+        //Final List
+        Collection finalList = new ArrayList();
+
+        //Variable that has the position TOP RIGHT from bishop
+        ChessPosition topRight = new ChessPosition(position.getRow()+1, position.getColumn()+1);
+        //Variable that has the position TOP LEFT from bishop
+        ChessPosition topLeft = new ChessPosition(position.getRow()+1, position.getColumn()-1);
+        //Variable that has the position BOTTOM RIGHT from bishop
+        ChessPosition bottomRight = new ChessPosition(position.getRow()-1, position.getColumn()+1);
+        //Variable that has the position BOTTOM LEFT from bishop
+        ChessPosition bottomLeft = new ChessPosition(position.getRow()-1, position.getColumn()-1);
+        //Variable that has the position TOP
+        ChessPosition top = new ChessPosition(position.getRow()+1, position.getColumn()+0);
+        //Variable that has the position BOTTOM
+        ChessPosition  bottom = new ChessPosition(position.getRow()-1, position.getColumn()+0);
+        //Variable that has the position  RIGHT
+        ChessPosition right = new ChessPosition(position.getRow()+0, position.getColumn()+1);
+        //Variable that has the position  LEFT
+        ChessPosition left = new ChessPosition(position.getRow()+0, position.getColumn()-1);
+
+
+        //TOP
+        //Checkins is inside board
+        if (isInside(top)){
+            moveChecker(finalList,board,position,top,1,0);
+        }
+
+        //TOP RIGHT
+        //Checkins is inside board
+        if (isInside(topRight)){
+            moveChecker(finalList,board,position,topRight,1,1);
+        }
+
+        //TOP LEFT
+        if(isInside(topLeft)){
+            moveChecker(finalList,board,position,topLeft,1,-1);
+        }
+
+        //BOTTOM
+        if(isInside(bottom)){
+            moveChecker(finalList,board,position,bottom,-1,0);
+        }
+
+        //BOTTOM RIGHT
+        if(isInside(bottomRight)){
+            moveChecker(finalList,board,position,bottomRight,-1,1);
+        }
+
+        //BOTTOM LEFT
+        if (isInside(bottomLeft)){
+            moveChecker(finalList,board,position,bottomLeft,-1,-1);
+        }
+
+        //RIGHT
+        if(isInside(right)){
+            moveChecker(finalList,board,position,right,0,1);
+        }
+
+        //LEFT
+        if (isInside(left)){
+            moveChecker(finalList,board,position,left,0,-1);
+        }
+
+        return finalList;
+    }
+
     //moveChecker FUNCTION - I used this one for all of the possible moves (top,topRight,topLeft,bottom,bottomRight,bottomLeft,right, left)
     public void moveChecker (Collection listOfMoves,ChessBoard board,ChessPosition currentPosition, ChessPosition positionX, int row, int col){
         //Possible New Position
@@ -111,73 +179,5 @@ public class CalcQueen extends PieceMovesCalculator {
             ChessMove pmoveX = new ChessMove(currentPosition,positionX,null);
             listOfMoves.add(pmoveX);
         }
-    }
-
-    @Override
-    public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position){
-        //Final List
-        Collection finalList = new ArrayList();
-
-        //Variable that has the position TOP RIGHT from bishop
-        ChessPosition topRight = new ChessPosition(position.getRow()+1, position.getColumn()+1);
-        //Variable that has the position TOP LEFT from bishop
-        ChessPosition topLeft = new ChessPosition(position.getRow()+1, position.getColumn()-1);
-        //Variable that has the position BOTTOM RIGHT from bishop
-        ChessPosition bottomRight = new ChessPosition(position.getRow()-1, position.getColumn()+1);
-        //Variable that has the position BOTTOM LEFT from bishop
-        ChessPosition bottomLeft = new ChessPosition(position.getRow()-1, position.getColumn()-1);
-        //Variable that has the position TOP
-        ChessPosition top = new ChessPosition(position.getRow()+1, position.getColumn()+0);
-        //Variable that has the position BOTTOM
-        ChessPosition  bottom = new ChessPosition(position.getRow()-1, position.getColumn()+0);
-        //Variable that has the position  RIGHT
-        ChessPosition right = new ChessPosition(position.getRow()+0, position.getColumn()+1);
-        //Variable that has the position  LEFT
-        ChessPosition left = new ChessPosition(position.getRow()+0, position.getColumn()-1);
-
-
-        //TOP
-        //Checkins is inside board
-        if (isInside(top)){
-            moveChecker(finalList,board,position,top,1,0);
-        }
-
-        //TOP RIGHT
-        //Checkins is inside board
-        if (isInside(topRight)){
-            moveChecker(finalList,board,position,topRight,1,1);
-        }
-
-        //TOP LEFT
-        if(isInside(topLeft)){
-            moveChecker(finalList,board,position,topLeft,1,-1);
-        }
-
-        //BOTTOM
-        if(isInside(bottom)){
-            moveChecker(finalList,board,position,bottom,-1,0);
-        }
-
-        //BOTTOM RIGHT
-        if(isInside(bottomRight)){
-            moveChecker(finalList,board,position,bottomRight,-1,1);
-        }
-
-        //BOTTOM LEFT
-        if (isInside(bottomLeft)){
-            moveChecker(finalList,board,position,bottomLeft,-1,-1);
-        }
-
-        //RIGHT
-        if(isInside(right)){
-            moveChecker(finalList,board,position,right,0,1);
-        }
-
-        //LEFT
-        if (isInside(left)){
-            moveChecker(finalList,board,position,left,0,-1);
-        }
-
-        return finalList;
     }
 }
