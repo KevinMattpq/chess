002cdc7e59d8202971cd02f423c8c19478074baf
Kevin Pena <kevinmpena05@gmail.com>
2026-03-10 23:45:28 -0600

    Working on MySQLUsers

--- server/src/main/java/dataaccess/MySQLUsers.java
+++ server/src/main/java/dataaccess/MySQLUsers.java
@@ -1,24 +1,31 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.UserData;
+import server.service.ResponseException;
 
-public class MySQLUsers {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-    public void addUser(UserData userData) throws ResponseException {
+public class MySQLUsers implements DAOUsersInterface{
+
+    public void createUser(UserData userData) throws ResponseException {
         var statement = "INSERT INTO user (name, type, json) VALUES (?, ?, ?)";
-        String json = new Gson().toJson(pet);
-        int id = executeUpdate(statement, pet.name(), pet.type(), json);
-        return new Pet(id, pet.name(), pet.type());
+        String json = new Gson().toJson(userData);
+        String username = executeUpdate(statement, userData.username(), json);
+        //return new UserData(userData.username());
     }
 
-    public void getUser(String username) throws ResponseException {
+    public UserData readUser(String username) throws ResponseException {
         try (Connection conn = DatabaseManager.getConnection()) {
-            var statement = "SELECT id, json FROM pet WHERE id=?";
+            var statement = "SELECT username, json FROM users WHERE username = username";
             try (PreparedStatement ps = conn.prepareStatement(statement)) {
                 ps.setInt(1, id);
                 try (ResultSet rs = ps.executeQuery()) {
                     if (rs.next()) {
-                        return readPet(rs);
+                        return readUser(rs);
                     }
                 }
             }
@@ -29,6 +36,33 @@ public class MySQLUsers {
 
     public void deleteAllUsers() throws ResponseException {
         var statement = "DELETE FROM users";
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  user (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(type),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws ResponseException, DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 
 }
